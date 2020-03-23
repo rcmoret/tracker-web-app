c329e6949e19e4e618e93f5e77cf4548c34ca958
Rusty Moret <rmoret@covermymeds.com>
2020-03-22 20:30:40 -0400

    Apollo client

---
+++ src/functions/ApolloClient.js
@@ -0,0 +1,15 @@
+import { ApolloClient } from "apollo-boost"
+import { HttpLink } from "apollo-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { API_URL } from "../constants/ApiConfig"
+
+const endpoint = '/api/graphql'
+
+const link = new HttpLink({
+  uri: `${API_URL}${endpoint}`
+})
+
+export const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+})
