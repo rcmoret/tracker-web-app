87a234f1d975c61be7576ee8e41a75517e71671c
Rusty Moret <rmoret@covermymeds.com>
2020-03-23 19:29:09 -0400

    Remove the api for the apollo clients url builder

--- src/functions/ApolloClient.js
+++ src/functions/ApolloClient.js
@@ -3,7 +3,7 @@ import { HttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { API_URL } from "../constants/ApiConfig"
 
-const endpoint = '/api/graphql'
+const endpoint = '/graphql'
 
 const link = new HttpLink({
   uri: `${API_URL}${endpoint}`
