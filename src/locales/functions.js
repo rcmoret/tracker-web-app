3df62537ea72b8d58bc112d0581e175dccf2b670
Rusty Moret <rmoret@covermymeds.com>
2020-04-01 18:48:14 -0400

    Update to titleize method

--- src/locales/functions.js
+++ src/locales/functions.js
@@ -1,7 +1,7 @@
 export const safePossessive = (name) => name === "" ? "" : `${name}'s`
 
 export const titleize = (string) => {
-  const words = string.split(/(\s|-|\/)/)
+  const words = string.split(/(\s|-|\(|\/)/)
   return words.map(word =>
     word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
   ).join("")
