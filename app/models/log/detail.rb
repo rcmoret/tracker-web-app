9f0c9ef78de8d66d04acf2feb20440e24419d1f8
Rusty Moret <rmoret@covermymeds.com>
2020-04-07 21:05:12 -0400

    Update to rubocop config

--- app/models/log/detail.rb
+++ app/models/log/detail.rb
@@ -4,6 +4,7 @@ module Log
   class Detail < ActiveRecord::Base
     belongs_to :entry, foreign_key: :log_entry_id
     belongs_to :type, class_name: 'DetailType', foreign_key: :log_entry_type_id
+    validates :log_entry_type_id, presence: true
     alias_attribute :entry_id, :log_entry_id
     alias_attribute :type_id, :log_type_id
 
